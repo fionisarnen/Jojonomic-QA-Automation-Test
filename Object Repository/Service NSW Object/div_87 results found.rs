af1280d7c71de589447cc51373ecbe50ef40bdce
Fioni <Fioni@DESKTOP-RP318JO>
2021-03-27 18:48:28 +0700

    add test components

---
+++ Object Repository/Service NSW Object/div_87 results found.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_87 results found</name>
+   <tag></tag>
+   <elementGuidId>f7ae1110-7a20-42c3-aaac-0f0413f3dc9a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='search']/div/div</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.search__results</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>search__results</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>87 results found for ‘apply for a number plate’Apply for a number plateIf you'd like new number plates, with a different style or character/number combination from your present plates, you can order and pay for these online, through myPlates.Apply to collect number plates from another service centreIf your number plates have arrived at a service centre that's no longer convenient as a pick-up location, you can apply to have them moved to another service centre. A 'move plates...Apply to exchange existing number plates between vehiclesYou can exchange number plates between 2 currently registered vehicles. You can apply to swap plates at a service centre. Conditions apply if you're wanting to transfer special num...Renew the reservation (hold) period on number platesIf you've reserved number plates and received a notice that your storage period is due to expire, you can renew/extend your reservation (hold) online, through myPlates.Order special number plates (SNPs) as a giftYou can order special number plates (SNPs) for someone else as a gift. There are a variety of SNPs you can order, with options such as background colour, letters, numbers and style...Reserve your number platesYou can reserve your NSW number plates for later use. This means they'll be physically stored at a service centre until you want to use them. You can reserve general issue and spec...Apply for tourist vehicle platesIf you have a vehicle used for tourist, charter or long distance passenger services, you can apply for tourist vehicle plates. The vehicle must seat more than 12 adult people and:...Apply for a motor dealer agreementAs a licensed motor dealer, if you want authorisation to register vehicles, transfer and renew registration and order number plates, you'll need to apply for a dealer agreement. Yo...Apply for a Dealer Vehicle Registration Scheme (DVRS) agreementAs a licensed motor dealer, if you want authorisation to register vehicles, transfer and renew registration and order number plates, you'll need to apply for a dealer agreement und...Number platesBrowse information about Number plates.Show more</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;search&quot;)/div[@class=&quot;search&quot;]/div[@class=&quot;search__results&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='search']/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[2]/following::div[15]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Home'])[3]/following::div[15]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//section/div/div/div/div/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Service NSW Object/h2_Service NSW locations.rs
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_Service NSW locations</name>
+   <tag></tag>
+   <elementGuidId>16bd72e7-b2be-43e3-b6f3-497b627baa77</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='locatorListView']/div/h2</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.locator__results > h2</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>6 Service NSW locations</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;locatorListView&quot;)/div[@class=&quot;locator__results&quot;]/h2[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='locatorListView']/div/h2</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Map view'])[1]/following::h2[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='List view'])[1]/following::h2[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Haymarket Service Centre'])[1]/preceding::h2[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Wynyard Service Centre'])[1]/preceding::h2[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[2]/div/div/h2</value>
+   </webElementXpaths>
+</WebElementEntity>
